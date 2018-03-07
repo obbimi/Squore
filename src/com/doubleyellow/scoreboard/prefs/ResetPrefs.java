239f2f79ae91d9eb39585a0f6efe3d570158311e
Iddo Hoeve <iddo.hoeve@is4u.be>
2018-03-07 15:50:34 +0100

    add missing icons for settings screen

--- src/com/doubleyellow/scoreboard/prefs/ResetPrefs.java
+++ src/com/doubleyellow/scoreboard/prefs/ResetPrefs.java
@@ -38,6 +38,7 @@ public class ResetPrefs extends DialogPreference
         super(context, attrs);
 
         this.context = context;
+	super.setDialogIcon(R.drawable.settings_reset);
     }
 
     @Override public void onClick(DialogInterface dialog, int iPressed)
